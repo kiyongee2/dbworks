2a90ca1abe3914dc605950a9eeac5c8cb87c0264
kiyongee2 <kiyongee2@gmail.com>
2023-04-13 07:52:19 +0900

    sqld function

--- hidb/dept_emp.sql
+++ hidb/dept_emp.sql
@@ -22,6 +22,9 @@ INSERT INTO department VALUES (20, '총무팀', '인천');
 -- 사원 자료 추가
 INSERT INTO employee VALUES (101, '이강', 27, 2500000, 10);
 INSERT INTO employee VALUES (102, '김산', 28, 2000000, 20);
+INSERT INTO employee VALUES (103, '박대양', 27, 3500000, 10);
+INSERT INTO employee VALUES (104, '신세계', 28, 1000000, 20);
+INSERT INTO employee VALUES (105, '강남', 28, 2000000, 20);
 INSERT INTO employee VALUES (103, '정들', 35, 3000000, 30); -- 부서코드 없음
 
 -- 부서 삭제
@@ -30,10 +33,12 @@ DELETE FROM department WHERE deptid = 20; -- employee 테이블에서 참조하고 있어 �
 ALTER TABLE employee DROP CONSTRAINT EMP_FK;
 
 SELECT * FROM employee;
+SELECT * FROM department;
 
 -- 테이블 삭제 - CONSTRAINT가 설정되어 있는 경우
 DROP TABLE department CASCADE CONSTRAINTS;
 
+DROP TABLE department;
 DROP TABLE employee;
 
 DELETE FROM employee;
