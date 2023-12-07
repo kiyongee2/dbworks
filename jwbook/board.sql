9d7a9164690d15ea29c7866088304e3b1d2506f2
kiyongee2 <kiyongee2@gmail.com>
2023-12-08 08:06:08 +0900

    board page

--- jwbook/board.sql
+++ jwbook/board.sql
@@ -14,13 +14,44 @@ CREATE TABLE board(
 
 CREATE SEQUENCE seq_bno NOCACHE;
 
-INSERT INTO board (bno, title, content, filename, id)
-VALUES (seq_bno.NEXTVAL, '글제목', '글내용입니다.', 'bird.jpg', 'khit');
+INSERT INTO board (bno, title, content, id)
+VALUES (seq_bno.NEXTVAL, '글제목', '글내용입니다.', 'khit');
+INSERT INTO board (bno, title, content, id)
+VALUES (seq_bno.NEXTVAL, '글제목2', '글내용2입니다.', 'khit');
+INSERT INTO board (bno, title, content, id)
+VALUES (seq_bno.NEXTVAL, '글제목3', '글내용3입니다.', 'today');
+INSERT INTO board (bno, title, content, id)
+VALUES (seq_bno.NEXTVAL, '글제목4', '글내용4입니다.', 'today');
 
-COMMIT;
+-- 재귀 복사
+INSERT INTO board (bno, title, content, id)
+(SELECT seq_bno.NEXTVAL, title, content, id FROM board);
 
-SELECT * FROM board ORDER BY bno DESC;
+-- 페이지 처리
+SELECT ROWNUM, board.*
+FROM board
+WHERE ROWNUM > 0 AND ROWNUM <= 10;
 
+-- 서브쿼리(검색이 불완전함)
+SELECT *
+FROM (SELECT ROWNUM RN, board.* FROM board ORDER BY bno DESC)
+WHERE RN >= 21 AND RN <= 30;
+
+-- 서브쿼리(deep 2단계- 주의! board 별칭을 bo를 사용함)
+SELECT *
+FROM (SELECT ROWNUM RN, bo.*
+      FROM (SELECT * FROM board ORDER BY bno DESC) bo)
+WHERE RN >= 11 AND RN <= 20;
+
+-- 총 게시글 수
+SELECT COUNT(*) AS total FROM board;
+
+-- 게시글 검색(제목, 아이디)
+SELECT *
+FROM board
+WHERE title LIKE '%2%';
+
+-- 댓글
 SELECT * FROM reply WHERE bno = 1;
 
 -- 댓글 수 계산하기
@@ -42,6 +73,9 @@ SET b.replycnt = (
 SELECT count(rno) 
 FROM reply
 WHERE bno = 1;
+
+DELETE FROM board;
+DELETE FROM reply;
 
 DROP TABLE board;
 DROP SEQUENCE seq_bno;
